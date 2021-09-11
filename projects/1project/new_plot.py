b12100dc42909dc806e9318068ac002c79932ace
fridtjrg <fridtjof@gjengset.com>
2021-09-11 19:35:17 +0200

    Combined to one program and updated plotting

---
+++ projects/1project/new_plot.py
@@ -0,0 +1,159 @@
+import matplotlib.pyplot as plt
+from math import *
+import numpy as np
+import sys
+
+
+#defines x, u and v lists
+x =[]
+u =[]
+v =[]
+
+#--------------Task 7 comparing u anv v-----------------
+
+with open("xandu_values_n=100000.txt") as myfile: #opens file
+    for line in myfile:                 #reads each line of the file
+        mydata = line.split(", ")       #splits each line with the seperator
+        x.append(float(mydata[0]))      #Appends values to x list
+        u.append(float(mydata[1]))      #Appends values to u list
+myfile.close()
+
+plt.plot(x,u, label ="u values (n=100000)")   
+
+x=[]
+
+for i in range(1,5):
+    n= 10**i
+
+    with open("xandv_values_n="+str(n)+".txt") as myfile: #opens file
+        for line in myfile:                 #reads each line of the file
+            mydata = line.split(", ")       #splits each line with the seperator
+            x.append(float(mydata[0]))
+            v.append(float(mydata[1]))      
+
+    myfile.close()  #closes file
+
+
+    x = np.array(x)
+    v = np.array(v)
+
+
+    plt.plot(x,v, label ="v values (n="+str(n)+")")
+
+    #empties lists for next itteration
+    x=[]
+    v=[]
+
+plt.xlabel("x-values")
+plt.ylabel("u and v values")
+plt.title("Comparison between u and v values")
+plt.legend()
+plt.grid()     
+plt.savefig('u_and_v_plotls.pdf')
+plt.show()    
+
+x =[]
+u =[]
+v =[] 
+
+#-------------- Task 8: plots the log(abs) error--------------
+
+
+for i in range(1,5):
+    n= 10**i
+
+
+    with open("xandu_values_n="+str(n)+".txt") as myfile: #opens file
+        for line in myfile:                 #reads each line of the file
+            mydata = line.split(", ")       #splits each line with the seperator
+            x.append(float(mydata[0]))      #Appends values to x list
+            u.append(float(mydata[1]))      #Appends values to u list
+    myfile.close() 
+
+
+    with open("xandv_values_n="+str(n)+".txt") as myfile: #opens file
+        for line in myfile:                 #reads each line of the file
+            mydata = line.split(", ")       #splits each line with the seperator
+            v.append(float(mydata[0]))
+
+    myfile.close()  #closes file
+
+    #converts to arrays
+    x = np.array(x)
+    v = np.array(v)
+    u = np.array(u)
+
+    #calculates the absolute error
+    log_abs_error = np.log10(abs(u-v))
+
+    #plots function u(x) 
+    plt.plot(x,log_abs_error, label ="$log_{10}(\Delta)$(n="+str(n)+")")
+
+    x=[]
+    v=[]
+    u=[]
+
+
+
+plt.xlabel("x-values")
+plt.ylabel("$log_{10}(\Delta)$")
+plt.title("Plot of the absolute error")
+plt.legend()
+plt.grid()     
+plt.savefig('abs-error(n='+str(n)+').pdf')
+plt.show()    
+
+
+
+#-------------Task 8: plots the realtive error--------------
+
+
+
+
+
+for i in range(1,5):
+    n= 10**i
+
+
+    with open("xandu_values_n="+str(n)+".txt") as myfile: #opens file
+        for line in myfile:                 #reads each line of the file
+            mydata = line.split(", ")       #splits each line with the seperator
+            x.append(float(mydata[0]))      #Appends values to x list
+            u.append(float(mydata[1]))      #Appends values to u list
+    myfile.close() 
+
+
+    with open("xandv_values_n="+str(n)+".txt") as myfile: #opens file
+        for line in myfile:                 #reads each line of the file
+            mydata = line.split(", ")       #splits each line with the seperator
+            v.append(float(mydata[0]))
+
+    myfile.close()  #closes file
+
+    """Encountered divide by zero problem"""
+    x = np.array(x[1:-1])
+    v = np.array(v[1:-1])
+    u = np.array(u[1:-1])
+
+
+
+    log_abs_error = np.log10(abs((u-v)/u))
+
+    #plots function u(x) 
+    plt.plot(x,log_abs_error, label ="$log_{10}(\epsilon)$(n="+str(n)+")")
+
+    x=[]
+    v=[]
+    u=[]
+
+
+
+
+plt.xlabel("x-values")
+plt.ylabel("$log_{10}(\epsilon$)")
+plt.title("Plot of the realtive error")
+plt.legend()
+plt.grid()     
+plt.savefig('rel-error(n='+str(n)+').pdf')
+#plt.show()    
+
---
+++ projects/1project/project1.cpp
@@ -0,0 +1,185 @@
+
+#include <iostream>
+#include <fstream> //allows for reading and writing files
+#include <string> //includes sting library
+#include <math.h>
+#include <iomanip>
+#include <sstream>
+#include <chrono>
+
+using namespace std;
+
+int calculate_v(int n);
+int create_u(int n);
+
+
+int main(){
+int n;
+
+cout<< "n=";
+cin >> n;
+
+create_u(n);
+calculate_v(n);
+
+}
+
+
+
+int calculate_v(int n){
+
+//------------Task 7------------
+  
+
+
+
+double x[n], u[n];
+
+ostringstream myreadfilename;
+myreadfilename << "xandu_values_n=" << n <<".txt";
+
+
+//reads values from task2
+ifstream myreadfile(myreadfilename.str());
+string sep = ", ", myText; //defines seperator from task 2
+
+//reads and inserts values for u and x
+int i = 0;
+while (getline (myreadfile, myText)) {
+  x[i] = stod(myText.substr(0, myText.find(sep))); 
+  u[i] = stod(myText.substr(myText.find(sep) + sep.length() , -1)); 
+  i++;
+
+}
+
+myreadfile.close();
+
+
+
+//defines and fills out g
+double g[n];
+
+float h = 1./n; //steplength
+
+for (int i=1; i<=n-1; i++){ 
+
+  //orginally: g[i] = (u[i-1]-(2*u[i])+u[i+1])/(pow(h,2))
+  g[i] = -(u[i-1]-(2*u[i])+u[i+1]); 
+
+  }
+
+
+
+
+
+double a[n-1];  //subdiagonal
+double b[n];    //main diagonal
+double c[n-1];  //superdiagonal
+
+//filling A matrix
+fill_n(a,n-1,-1);
+fill_n(b,n,2);
+fill_n(c,n-1,-1);
+
+//---------------task 6 algorithm----------------------
+
+double v[n];           //Vector v, same size as b
+
+
+double b_tilda[n], g_tilda[n];
+
+
+// by Gaussian elmination:
+
+b_tilda[1] = b[1]; g_tilda[1] = g[1]; //special cases
+
+
+auto t1 = std::chrono::high_resolution_clock::now();
+//forward subst. (total 6*(n-2) flops)
+for (int i=2; i<=n-1; i++){ // from 2 to be correct for g, to n-1 beacuse of indexing
+
+  b_tilda[i] = b[i] - ((a[i]/b_tilda[i-1])*c[i-1]);         //3 flops
+  g_tilda[i] = g[i] - ((a[i]/b_tilda[i-1])*g_tilda[i-1]);   //3 flops
+
+  }
+
+
+
+v[n-1] = g_tilda[n-1]/b_tilda[n-1]; //special case for last elemtns in v(1 flop)
+
+
+//back subst. (total 3*(n-2) flops)
+for (int i=n-2; i>=1; i--){ // from n-2 to not go out of range
+
+      v[i] = (g_tilda[i] - (c[i]*v[i+1]))/(b_tilda[i]);     //3 flops
+  }
+
+v[0] = 0; v[n]=0; //applying same boundaries as for u
+
+auto t2 = std::chrono::high_resolution_clock::now();
+   
+double duration_seconds = std::chrono::duration<double>(t2 - t1).count();
+
+cout << endl << "the calculation took " << duration_seconds <<"s"<<endl;
+
+/*
+For some reason, dividing v by -1100 gives me 
+a graph that looks like u(x), i suspect i have some kind
+of typo in my code that i can't spot
+*/
+
+
+ofstream mywritefile;
+
+ostringstream myfilename;
+myfilename << "xandv_values_n=" << n <<".txt";
+
+
+mywritefile.open(myfilename.str());
+
+for (int i = 0; i<= n; i++){
+    mywritefile<< fixed<< setprecision(3) << x[i] << ", " << setprecision(4) << v[i]<< endl;
+}
+
+
+mywritefile.close();
+
+
+return 0;
+}
+
+
+int create_u(int n){
+//task 2
+
+
+double x[n]; //defines double precision array for x-values
+
+ostringstream myfilename;
+myfilename << "xandu_values_n=" << n <<".txt";
+
+ofstream myfile;
+
+myfile.open(myfilename.str());
+
+for (int i = 0; i< n; i++){
+
+    x[i] = (double) i/n;  //fills x[] with x-values between 0 and 1
+
+    //writes x and u values in file with ", " as sepperator
+    myfile<< fixed<< setprecision(3) << x[i] << ", " << setprecision(8) << 1.0-(1.0-exp(-10.0))*x[i] - exp(-10.0*x[i])<< endl;
+}
+
+myfile << 1 << ", " << 0.0 << endl; //special condition for boundary
+
+myfile.close();
+
+
+return 0;
+}
+
+
+
+
+
+
